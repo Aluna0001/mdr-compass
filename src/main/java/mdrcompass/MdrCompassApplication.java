6a6904ea95d217029052137c4802d2580fc86755
Luna <cocobuntus@gmail.com>
2026-05-30 16:48:30 +0200

    initial commit

---
+++ src/main/java/mdrcompass/MdrCompassApplication.java
@@ -0,0 +1,13 @@
+package mdrcompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdrCompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdrCompassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mdrcompass/MdrCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package mdrcompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdrCompassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
