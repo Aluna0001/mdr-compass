244f72ce17f25ddce4045a9812110d2adad73617
Luna <cocobuntus@gmail.com>
2026-06-16 10:16:48 +0200

    Add news endpoint fetching and parsing The Hacker News RSS feed

---
+++ src/main/java/mdrcompass/controller/NewsController.java
@@ -0,0 +1,25 @@
+package mdrcompass.controller;
+
+import mdrcompass.model.NewsItem;
+import mdrcompass.service.NewsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+
+    private final NewsService newsService;
+
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @GetMapping
+    public List<NewsItem> getNews() {
+        return newsService.getNews();
+    }
+}
